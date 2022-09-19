125b22f62cd3a208aa432db820ace1d6bd729923
shchnk1103 <761962425@qq.com>
2022-09-19 21:08:18 +0800

    add Bootstrap

--- frontend/src/views/LoginPages.vue
+++ frontend/src/views/LoginPages.vue
@@ -1,102 +1,132 @@
 <template>
-  <div class="main-page">
-    <!-- Left -->
-    <div class="left">
-      <div>
-        <img :src="images" alt="" class="login-img" />
+  <div class="container-fluid text-center main-page">
+    <div class="row justify-content-md-center">
+      <div class="col-1"></div>
+      <!-- Left -->
+      <div class="col-5 left align-items-center">
+        <img :src="images" alt="" class="img-fluid login-img" />
       </div>
-    </div>
-    <!-- Right -->
-    <div class="right">
-      <div class="right-page">
-        <img :src="InstagramImage" alt="" style="padding: 0px 70px 30px 70px" />
-        <input
-          v-model="user_email"
-          placeholder="Please enter your email."
-          type="text"
-        />
-        <input
-          v-model="user_password"
-          placeholder="Please enter your password"
-          type="password"
-        />
-        <!-- Login Button -->
-        <div
-          @click="login()"
-          style="
-            background-color: rgba(0, 149, 246, 0.3);
-            text-align: center;
-            color: white;
-            margin-top: 10px;
-            border-radius: 5px;
-            padding: 10px;
-          "
-        >
-          Log In
-        </div>
-        <!-- OR -->
-        <div
-          style="
-            display: flex;
-            margin-top: 30px;
-            flex-direction: row;
-            align-items: center;
-          "
-        >
+      <!-- Right -->
+      <div class="col-5 align-items-center">
+        <div class="right-page col needs-validation">
+          <img
+            :src="InstagramImage"
+            alt=""
+            style="padding: 0px 70px 30px 70px"
+          />
+          <form class="needs-validation" novalidate>
+            <!-- user_email -->
+            <div class="btn-email">
+              <input
+                v-model="user_email"
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Please enter your email."
+                required
+              />
+              <div class="invalid-feedback">Please enter a email.</div>
+            </div>
+            <!-- user_password -->
+            <div class="btn-password">
+              <input
+                v-model="user_password"
+                type="password"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Please enter your password"
+                required
+              />
+              <div class="invalid-feedback">Please enter a password.</div>
+            </div>
+          </form>
+          <!-- Login Button -->
           <div
+            @click="login()"
             style="
-              background-color: rgb(219, 219, 219);
-              height: 1px;
-              width: 142px;
+              background-color: rgba(0, 149, 246, 0.3);
+              text-align: center;
+              color: white;
+              margin-top: 10px;
+              border-radius: 5px;
+              padding: 10px;
             "
-          ></div>
-          <div
-            style="font-weight: 600; color: gray; padding: 0px 20px 0px 20px"
+            type="submit"
           >
-            OR
+            Log In
           </div>
+          <!-- OR -->
           <div
             style="
-              background-color: rgb(219, 219, 219);
-              height: 1px;
-              width: 142px;
+              display: flex;
+              margin-top: 30px;
+              flex-direction: row;
+              align-items: center;
             "
-          ></div>
+          >
+            <div
+              style="
+                background-color: rgb(219, 219, 219);
+                height: 1px;
+                width: 142px;
+              "
+            ></div>
+            <div
+              style="font-weight: 600; color: gray; padding: 0px 20px 0px 20px"
+            >
+              OR
+            </div>
+            <div
+              style="
+                background-color: rgb(219, 219, 219);
+                height: 1px;
+                width: 142px;
+              "
+            ></div>
+          </div>
+          <!-- Log in with Facebook -->
+          <div
+            style="
+              text-align: center;
+              margin-top: 20px;
+              color: rgb(56, 81, 133);
+              font-weight: 600;
+              font-size: 18px;
+            "
+          >
+            <span>Log in with Facebook</span>
+          </div>
+          <!-- Forget password? -->
+          <div
+            style="
+              color: rgb(0, 55, 107);
+              text-align: center;
+              padding-top: 20px;
+            "
+          >
+            Forget password?
+          </div>
         </div>
-        <!-- Log in with Facebook -->
+        <!-- Don't have an account? -->
         <div
+          class="col"
           style="
+            background-color: white;
+            border: 1px solid rgb(219, 219, 219);
             text-align: center;
+            padding: 20px;
+            margin: auto;
             margin-top: 20px;
-            color: rgb(56, 81, 133);
-            font-weight: 600;
-            font-size: 18px;
+            width: 350px;
           "
         >
-          <span>Log in with Facebook</span>
-        </div>
-        <!-- Forget password? -->
-        <div
-          style="color: rgb(0, 55, 107); text-align: center; padding-top: 20px"
-        >
-          Forget password?
+          Don't have an account?
+          <span style="color: rgba(0, 149, 246, 1); font-weight: 600"
+            >Sign up?</span
+          >
         </div>
       </div>
-      <!-- Don't have an account? -->
-      <div
-        style="
-          background-color: white;
-          border: 1px solid rgb(219, 219, 219);
-          text-align: center;
-          padding: 20px;
-          margin: 20px;
-        "
-      >
-        Don't have an account?
-        <span style="color: rgba(0, 149, 246, 1); font-weight: 600"
-          >Sign up?</span
-        >
-      </div>
+      <div class="col-1"></div>
     </div>
   </div>
 </template>
@@ -144,21 +174,24 @@ export default {
 </script>
 
 <style scoped>
+.main-page {
+  padding-top: 10%;
+}
+
+.btn-email,
+.btn-password {
+  width: 248px;
+  margin-top: 10px;
+}
+
 input {
   all: unset;
   padding: 10px;
   border: 1px solid rgba(219, 219, 219);
-  margin-top: 10px;
+  border-radius: 4px;
   text-align: left;
-}
-
-.main-page {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 100%;
 }
 
 .left {
@@ -175,9 +208,10 @@ input {
 
 .login-img {
   position: relative;
-  top: 27px;
-  left: 157px;
-  width: 251px;
+  top: 1%;
+  left: 35.5%;
+  width: 260px;
+  height: 540px;
 }
 
 .right-page {
@@ -191,6 +225,7 @@ input {
   background-color: white;
   border: 1px solid rgb(219, 219, 219);
   padding: 40px;
-  margin: 20px;
+  margin: auto;
+  margin-top: 20px;
 }
 </style>
